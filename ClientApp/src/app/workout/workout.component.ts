35989cb10132a8d953672a0512a07fe638d8e02c
Carson <carson.sturtevant@gmail.com>
2019-02-12 23:07:08 -0600

    initial commit

---
+++ ClientApp/src/app/workout/workout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout',
+  templateUrl: './workout.component.html',
+  styleUrls: ['./workout.component.css']
+})
+export class WorkoutComponent {
+
+  constructor() { }
+    
+
+}
